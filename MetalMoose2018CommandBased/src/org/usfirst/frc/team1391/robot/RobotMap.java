f98702f716a99ad855fbeada74bf14bd04cc2b07
xiaoxiae <tomas.slama.131@gmail.com>
2018-02-06 16:20:51 -0500

    change port variables in RobotMap.java to final and comment

--- MetalMoose2018CommandBased/src/org/usfirst/frc/team1391/robot/RobotMap.java
+++ MetalMoose2018CommandBased/src/org/usfirst/frc/team1391/robot/RobotMap.java
@@ -14,27 +14,30 @@ package org.usfirst.frc.team1391.robot;
  */
 public class RobotMap {
 	/** CONTROLLER MAPPING **/
-	public static int controllerPort = 0;
+	//Default controller port
+	public static final int controllerPort = 0;	
+	
+	//Ports for the Y axes of the two joysticks of the Logitech controller
+	public static final int tankDriveLeftStickYAxisPort = 1;
+	public static final int tankDriveRightStickYAxisPort = 5;
+	
+	//Port of the rotation axis of the Logitech joystick
+	public static final int arcadeDriveRotationAxisPort = 2;
 
 	/** MOTOR MAPPING **/
-	public static int leftMotor1Port = 0;
-	public static int leftMotor2Port = 1;
-	public static int leftMotor3Port = 2;
+	//When looking at the left side of the robot, the motors go counter-clockwise form the top one.
+	public static final int leftMotor1Port = 0;
+	public static final int leftMotor2Port = 1;
+	public static final int leftMotor3Port = 2;
 
-	public static int rightMotor1Port = 3;
-	public static int rightMotor2Port = 4;
-	public static int rightMotor3Port = 5;
+	//When looking at the right side of the robot, the motors go counter-clockwise form the top one.
+	public static final int rightMotor1Port = 3;
+	public static final int rightMotor2Port = 4;
+	public static final int rightMotor3Port = 5;
 	
-	/** MISC. **/
-	//Switching drive modes (tank == 0, arcade == 1, joystick = 2)
+	/** MISCELLANEOUS **/
+	//Switching drive modes
 	//0 is tank drive and 1 is arcade drive with the Logitech controller
 	//2 uses the Y axis and the rotation axis from the joystick controller
 	public static int driveMode = 2;
-	
-	//Ports for the Y axes of the two joysticks of the Logitech controller
-	public static int tankDriveLeftStickYAxisPort = 1;
-	public static int tankDriveRightStickYAxisPort = 5;
-	
-	//Port of the rotation axis of the Logitech joystick
-	public static int arcadeDriveRotationAxisPort = 2;
 }
