18d0f63d662e184e1b62c92f3628e26e0b054440
Unknown <tomas.slama.131@gmail.com>
2018-03-05 00:07:03 -0500

    add getThrottledSpeed method to Elevator.java
    This method performs the calculation of the value of the throttle function (currently of a 5th degree polynomial).

--- MetalMoose2018CommandBased/src/org/usfirst/frc/team1391/robot/subsystems/Elevator.java
+++ MetalMoose2018CommandBased/src/org/usfirst/frc/team1391/robot/subsystems/Elevator.java
@@ -1,6 +1,8 @@
 package org.usfirst.frc.team1391.robot.subsystems;
 
-import edu.wpi.first.wpilibj.*;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.Spark;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import org.usfirst.frc.team1391.robot.RobotMap;
 import org.usfirst.frc.team1391.robot.commands.ElevatorManualControl;
@@ -9,7 +11,7 @@ import org.usfirst.frc.team1391.robot.commands.ElevatorManualControl;
  * Controls the elevator.
  */
 public class Elevator extends Subsystem {
-    //
+    // Speed controllers for the motors of the elevator
     private Spark elevatorLeftMotor = new Spark(RobotMap.elevatorLeftMotorPort);
     private Spark elevatorRightMotor = new Spark(RobotMap.elevatorRightMotorPort);
     private SpeedControllerGroup elevatorMotors = new SpeedControllerGroup(elevatorLeftMotor, elevatorRightMotor);
@@ -17,26 +19,62 @@ public class Elevator extends Subsystem {
     public Encoder elevatorEncoder = new Encoder(RobotMap.elevatorEncoderAPort, RobotMap.elevatorEncoderBPort, false, Encoder.EncodingType.k4X);
 
     public Elevator() {
-        elevatorEncoder.setDistancePerPulse(RobotMap.elevatorEncoderCoefficient);
+        // The motors have to run in opposite directions.
         elevatorLeftMotor.setInverted(true);
-    }
 
-    public void setThrottledSpeed(double speed) {
-        double elevatorPosition = elevatorEncoder.getDistance();
-
-        // Calculates the progressively slower elevator speed
-        double elevatorSpeed = -speed * ((-(elevatorPosition * elevatorPosition) / 264) + (17 * elevatorPosition) / 132);
-    }
-
-    public void setAbsoluteSpeed(double speed) {
-        elevatorMotors.set(speed);
-    }
-
-    public void hold() {
-        elevatorMotors.set(RobotMap.elevatorHoldSpeed);
+        // Change the coefficient of the elevator
+        elevatorEncoder.setDistancePerPulse(RobotMap.elevatorEncoderCoefficient);
     }
 
     public void initDefaultCommand() {
         setDefaultCommand(new ElevatorManualControl());
+    }
+
+    /**
+     * Set elevator speed, multiplied by the throttle function depending on the position of the elevator.
+     *
+     * @param speed Input form the joystick.
+     */
+    public void setThrottledSpeed(double speed) {
+        double elevatorPosition = elevatorEncoder.getDistance();
+        double throttledSpeed = speed * getThrottledSpeed(elevatorPosition);
+
+        elevatorMotors.set(throttledSpeed);
+    }
+
+    /**
+     * Get the y value at point x of the graph.
+     *
+     * @param x The x of the graph.
+     * @return The y of the graph.
+     */
+    public double getThrottledSpeed(double x) {
+        // The coefficients of the polynomial
+        double[] coefficients = new double[]{-0.0000071507642949, 0.0004862519720525, -0.0117169374510997, 0.1173222334909840, 0.6000000000006800};
+
+        // Calculate the y value at point x of the polynomial
+        // Example for 4th degree polynomial: ax^3 + bx^2 + cx + d = x(x(x(a) + b) + c) + d... this simplifies the calculation
+        double value = 0;
+        for (int i = 0; i < coefficients.length; i++) value = value * x + coefficients[i];
+
+        // The maximum motor power is 1... it does not make sense to have more than that.
+        if (value > 1) return 1;
+        else return value;
+    }
+
+    /**
+     * Set the absolute speed of the motors.
+     *
+     * @param speed Speed to set for the motors (-1 to 1).
+     */
+    public void setAbsoluteSpeed(double speed) {
+        elevatorMotors.set(speed);
+    }
+
+    /**
+     * Sets the motors to the hold speed.
+     */
+    public void hold() {
+        elevatorMotors.set(RobotMap.elevatorHoldSpeed);
     }
 }
