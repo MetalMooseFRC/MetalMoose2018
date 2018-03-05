e3c3b63dd9447411900210cfff9c9b112b5d4fd2
Unknown <tomas.slama.131@gmail.com>
2018-03-04 21:11:24 -0500

    change the format of the parsed commandString
    The format changed from Command:parameter:parameter:... to Command(parameter, parameter,...).

--- MetalMoose2018CommandBased/src/org/usfirst/frc/team1391/robot/commands/AutonomousCommandGroup.java
+++ MetalMoose2018CommandBased/src/org/usfirst/frc/team1391/robot/commands/AutonomousCommandGroup.java
@@ -5,45 +5,46 @@ import org.usfirst.frc.team1391.robot.RobotMap;
 
 /**
  * Takes a string consisting of g-code style command series and produces an
- * autonomous. Each command is separated by a space, and each argument of the
- * command is separated by a colon.
+ * autonomous. See the markdown documentation for more details.
  */
 public class AutonomousCommandGroup extends CommandGroup {
 
     /**
      * Takes a String in the g-code style language and converts it into commands.
      *
-     * @param command The command to be parsed.
+     * @param commandString The command to be parsed.
      */
-    public AutonomousCommandGroup(String command) {
+    public AutonomousCommandGroup(String commandString) {
         // Removes whitespace from both ends of the String and changes all upper case chars to lower case
-        command = command.trim().toLowerCase();
+        // It also separates all individual commands with a newline symbol
+        commandString = commandString.trim().toLowerCase().replaceAll("\\)(\\s+)", ")\n");
 
         // The actual parsing of the command
-        if (command.length() != 0) parseCommand(command, false);
+        if (commandString.length() != 0) parseCommand(commandString, false);
     }
 
     /**
      * Parses the command String.
      *
-     * @param command  The String to be parsed.
-     * @param reversed Mirrors the angles, so there is no need for duplicate autonomodes
+     * @param commandString  The String to be parsed.
+     * @param reversed Mirrors commands (reverses angles and x axes).
      */
-    private void parseCommand(String command, boolean reversed) {
+    private void parseCommand(String commandString, boolean reversed) {
+        // Split the String into a list of commands
+        String[] commandList = commandString.split("\n");
 
-        String[] stepList = command.split(" ");
+        for (String command : commandList) {
 
-        for (String step : stepList) {
+            // Split to individual parameters (on any ", ", or on "(")
+            String[] commandParts = command.replaceAll("\\)", "").split("\\(+|((,)[\\s]*)+");
 
-            // Split to individual parameters
-            String[] stepParameterValues = step.split(":");
 
             // Takes the first character of the first argument, so we can explicitly write out the commands
-            switch (stepParameterValues[0].charAt(0)) {
+            switch (commandParts[0].charAt(0)) {
 
-                // Move:(distance in ft):(angle in degrees) - moves or turns the robot
+                // Move(distance in ft, angle in degrees) - moves or turns the robot
                 case 'm': {
-                    double distance = Double.parseDouble(stepParameterValues[1]);
+                    double distance = Double.parseDouble(commandParts[1]);
 
                     // If reversed is true, we need to invert the angle
                     double angle = Double.parseDouble(stepParameterValues[2]) * (reversed ? -1 : 1);
@@ -63,35 +64,35 @@ public class AutonomousCommandGroup extends CommandGroup {
                     break;
                 }
 
-                // Intake:(time in seconds) - parallel - intakes the cube
+                // Intake(time in seconds) - parallel - intakes the cube
                 case 'i': {
-                    double lengthOfIntake = Double.parseDouble(stepParameterValues[1]);
+                    double lengthOfIntake = Double.parseDouble(commandParts[1]);
 
                     addParallel(new CollectorIntake(lengthOfIntake));
                     break;
                 }
 
-                // Outtake:(time in seconds) - sequential - outtakes the cube
+                // Outtake(time in seconds) - sequential - outtakes the cube
                 case 'o': {
-                    double lengthOfOuttake = Double.parseDouble(stepParameterValues[1]);
+                    double lengthOfOuttake = Double.parseDouble(commandParts[1]);
 
                     addSequential(new CollectorOuttake(lengthOfOuttake));
                     break;
                 }
 
-                // Timeout:(time in seconds) - sequential timeout of the drivebase
+                // Timeout(time in seconds) - sequential timeout of the drivebase
                 case 't': {
-                    double lengthOfDrivebaseTimeout = Double.parseDouble(stepParameterValues[1]);
+                    double lengthOfDrivebaseTimeout = Double.parseDouble(commandParts[1]);
 
                     addSequential(new DrivebaseTimeout(lengthOfDrivebaseTimeout));
                     break;
                 }
 
-                // Goto:(x coordinate):(y coordinate) - generates two moves (turn, then drive) to move to the coordinate.
+                // Goto(x, y) - generates two moves (turn, then drive) to move to the coordinate.
                 case 'g': {
                     // If reversed is true, then we need to reverse x (the board is mirrored)
-                    double x = Double.parseDouble(stepParameterValues[1]) * (reversed ? -1 : 1);
-                    double y = Double.parseDouble(stepParameterValues[2]);
+                    double x = Double.parseDouble(commandParts[1]) * (reversed ? -1 : 1);
+                    double y = Double.parseDouble(commandParts[2]);
 
                     addSequential(new DriveAutonomous(x, y, true));
                     addSequential(new DriveAutonomous(x, y, false));
@@ -99,17 +100,17 @@ public class AutonomousCommandGroup extends CommandGroup {
                     break;
                 }
 
-                // -Chunk:(number of the chunk) - inverted chunk
+                // -Chunk(number of the chunk) - inverted chunk
                 case '-': {
-                    int chunkNumber = Integer.parseInt(stepParameterValues[1]);
+                    int chunkNumber = Integer.parseInt(commandParts[1]);
                     String chunk = RobotMap.chunks[chunkNumber];
 
                     parseCommand(chunk, !reversed);
                 }
 
-                // Chunk:(number of the chunk) - normal chunk
+                // Chunk(number of the chunk) - normal chunk
                 case 'c': {
-                    int chunkNumber = Integer.parseInt(stepParameterValues[1]);
+                    int chunkNumber = Integer.parseInt(commandParts[1]);
                     String chunk = RobotMap.chunks[chunkNumber];
 
                     parseCommand(chunk, reversed);
