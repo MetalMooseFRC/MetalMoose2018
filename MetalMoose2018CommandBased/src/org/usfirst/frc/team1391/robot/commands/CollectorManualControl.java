c8675665ceb8b83a2e3b2cbf6d763c54b4a1e705
Unknown <tomas.slama.131@gmail.com>
2018-03-29 19:06:34 -0400

    update collector manual to set collectorIntake after axis was released
    Up until now, an axis press in the CollectorManualControl set the collectorIntake variable immediately. This would not work with the future implementation of the clamp, so it was changed.

--- MetalMoose2018CommandBased/src/org/usfirst/frc/team1391/robot/commands/CollectorManualControl.java
+++ MetalMoose2018CommandBased/src/org/usfirst/frc/team1391/robot/commands/CollectorManualControl.java
@@ -9,7 +9,10 @@ import org.usfirst.frc.team1391.robot.RobotMap;
  * Controls the collector manually using axes on the operator controller.
  */
 public class CollectorManualControl extends Command {
-
+	// Value to watch for joystick release (for setting the hold of the cube)
+	boolean wasJoystickPressed = false;
+	boolean valueForCollectorIntake = false;
+	
     public CollectorManualControl() {
         requires(Robot.myCollector);
     }
@@ -23,19 +26,32 @@ public class CollectorManualControl extends Command {
 
         // Intakes the cube (the 0.1 is just an arbitrary threshold, because the joysticks' position is not 0)
         if (leftTriggerSpeed > 0.1) {
-            RobotMap.intakeWithCollector = true;
             Robot.myCollector.setAbsoluteSpeed(leftTriggerSpeed);
+
+            // We are basically watching for when we release the joystick
+            wasJoystickPressed = true;
+            valueForCollectorIntake = true;
         }
 
         // Outtakes the cube (the 0.1 is just an arbitrary threshold, because the joysticks' position is not 0)
         else if (rightTriggerSpeed > 0.1) {
-            RobotMap.intakeWithCollector = false;
             Robot.myCollector.setAbsoluteSpeed(-rightTriggerSpeed);
+            wasJoystickPressed = true;
+            valueForCollectorIntake = false;
+        } 
+        
+        // If there are no inputs from the axes
+        else {
+        	// If the joystick was just released
+            if (wasJoystickPressed) {
+            	RobotMap.intakeWithCollector = valueForCollectorIntake;
+            	wasJoystickPressed = false;
+            }
+            
+            // Else if we want to hold the cube (after intaking)
+            if (RobotMap.intakeWithCollector) Robot.myCollector.setAbsoluteSpeed(RobotMap.collectorHoldSpeed);
+        	else Robot.myCollector.setAbsoluteSpeed(0);
         }
-
-        // Else if we want to hold the cube (after intaking)
-        else if (RobotMap.intakeWithCollector) Robot.myCollector.setAbsoluteSpeed(RobotMap.collectorHoldSpeed);
-        else Robot.myCollector.setAbsoluteSpeed(0);
     }
 
     protected boolean isFinished() {
